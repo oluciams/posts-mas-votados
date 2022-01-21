4fe6dcbadafb0754197bcd3839a15d41ff050f9b
olga <oluciams84@gmail.com>
2022-01-21 13:23:48 -0500

    Commit inicial

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export const Header = () => {
+   
+return (    
+  <>
+    <h3 className='text-center mt-2'>Titlo</h3>
+
+  </>                                   
+    
+  )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+
+export const Home = () => {
+   
+return (    
+  <Container fluid>  
+    <h3 className='text-center mt-2'>Welcome</h3>                  
+    <Banner/>                                 
+  </Container>      
+  )
+}
